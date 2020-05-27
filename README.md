73b23e72b5c23ef4ab9a039a0873a4307e64c391
Michael Alls <michael.alls@fedex.com>
2020-05-27 14:24:51 -0400

    added edit note logic

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Google Keep Clone - Scrimba Tutorial
 
 A note is an object with an id, color, title, and text
+
+-[] Look into turning into a class object or constructor function.
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 class App {
   constructor() {
     this.notes = []; //Why did we put this here? couldn't it be outside constructor
+    this.title = '';
+    this.text = '';
+    this.id = '';
 
     console.log('app works!');
     this.$placeholder = document.querySelector('#placeholder');
@@ -9,12 +12,19 @@ class App {
     this.$noteTitle = document.querySelector('#note-title');
     this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
+    this.$formCloseButton = document.querySelector('#form-close-button');
+    this.$modal = document.querySelector('.modal');
+    this.$modalTitle = document.querySelector('.modal-title');
+    this.$modalText = document.querySelector('.modal-text');
+    this.$modalCloseButton = document.querySelector('.modal-close-button');
     this.addEventListeners();
   }
 
   addEventListeners() {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event);
+      this.selectNote(event);
+      this.openModal(event);
     });
 
     //submit listener done via form not button so enter can be used
@@ -28,13 +38,28 @@ class App {
         this.addNote({ title, text });
       }
     });
+
+    this.$formCloseButton.addEventListener('click', event => {
+      event.stopPropagation();
+      this.closeForm();
+    });
+
+    this.$modalCloseButton.addEventListener('click', event => {
+      this.closeModal(event);
+    });
   }
 
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
 
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+    const hasNote = title || text;
+
     if (isFormClicked) {
       this.openForm();
+    } else if (hasNote) {
+      this.addNote({ title, text });
     } else {
       this.closeForm();
     }
@@ -54,10 +79,23 @@ class App {
     this.$noteText.value = '';
   }
 
-  addNote(note) {
+  openModal(event) {
+    if (event.target.closest('.note')) {
+      this.$modal.classList.toggle('open-modal');
+      this.$modalTitle.value = this.title;
+      this.$modalText.value = this.text;
+    }
+  }
+
+  closeModal() {
+    this.editNote();
+    this.$modal.classList.toggle('open-modal');
+  }
+
+  addNote({ title, text }) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title,
+      text,
       color: 'white',
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
     };
@@ -67,6 +105,24 @@ class App {
     this.closeForm();
   }
 
+  editNote() {
+    const title = this.$modalTitle.value;
+    const text = this.$modalText.value;
+    this.notes = this.notes.map(note =>
+      note.id === Number(this.id) ? { ...note, title, text } : note
+    );
+    this.displayNotes();
+  }
+
+  selectNote(event) {
+    const $selectedNote = event.target.closest('.note');
+    if (!$selectedNote) return;
+    const [$noteTitle, $noteText] = $selectedNote.children;
+    this.title = $noteTitle.innerText;
+    this.text = $noteText.innerText;
+    this.id = $selectedNote.dataset.id;
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
@@ -74,7 +130,9 @@ class App {
     this.$notes.innerHTML = this.notes
       .map(
         note => `
-        <div style="background: ${note.color};" class="note">
+        <div style="background: ${note.color};" class="note" data-id="${
+          note.id
+        }">
           <div class="${note.title && 'note-title'}">${note.title}</div>
           <div class="note-text">${note.text}</div>
           <div class="toolbar-container">
